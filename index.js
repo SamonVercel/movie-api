314dc899b34c4cbafc1dde4f0931acf0bd73b498
SamonVercel <161924682+SamonVercel@users.noreply.github.com>
2024-08-28 13:02:22 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const cors = require("cors"); // Import cors package
 
 const app = express();
 
+// Apply CORS middleware
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500", // Allow requests from this origin
+    methods: ["GET", "POST"], // Specify allowed methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  })
+);
+
+// Define a simple route
 app.get("/", (req, res) => {
   res.send({
     account: [
@@ -15,9 +26,7 @@ app.get("/", (req, res) => {
   });
 });
 
+// Start the server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-
-// Export the Express API
-module.exports = app;
